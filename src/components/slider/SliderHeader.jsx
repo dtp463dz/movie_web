652c3ec77d1c308bdd9465e596af4f1e83f7e5e5
PhucDT <dinhthaiphuc463@gmail.com>
2026-06-27 13:16:37 +0700

    fix: fix reload in vercel and fix view more in SliderHeader

--- src/components/slider/SliderHeader.jsx
+++ src/components/slider/SliderHeader.jsx
@@ -1,18 +1,30 @@
-const SliderHeader = ({ title, onViewMore }) => {
+import { useNavigate } from "react-router-dom";
+
+const SliderHeader = ({ title, viewMoreLink, onViewMore }) => {
+    const navigate = useNavigate();
+
+    const handleViewMore = () => {
+        if (onViewMore) {
+            onViewMore();
+        } else if (viewMoreLink) {
+            navigate(viewMoreLink);
+        }
+    };
+
     return (
         <div className="flex justify-between items-center mb-4">
             <h2 className="text-lg text-blue-700 font-bold border-l-4 border-blue-500 pl-2">
                 {title}
             </h2>
-            <button
-                className="text-sm text-blue-400 hover:underline"
-                onClick={onViewMore}
-            >
-                XEM THÊM
-            </button>
+            {(viewMoreLink || onViewMore) && (
+                <button
+                    className="text-sm text-blue-400 cursor-pointer hover:text-blue-600 transition-colors"
+                    onClick={handleViewMore}
+                >
+                    XEM THÊM
+                </button>
+            )}
         </div>
-    )
-
-}
-
+    );
+};
 export default SliderHeader;
